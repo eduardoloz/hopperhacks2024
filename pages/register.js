80cdd7dd45236800109226a54a85f0551438f4b8
Samuel Buena <sbuena@cs.stonybrook.edu>
2024-02-03 17:03:05 -0500

    modijy register.js to post data

--- pages/register.js
+++ pages/register.js
@@ -4,7 +4,7 @@ import "../css/register.css";
 function Register() {
   return (
     <div class="register-form">
-      <form action="" class="form">
+      <form action="/register" method="post" class="form">
         <p class="register-title">Dreamy Hoppers Application</p>
         <div class="username-input">
           <input type="text" placeholder="Enter Username" />
