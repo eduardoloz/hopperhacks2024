8ecec2dcb0ce2179d48e150d473775f9e378d28a
Samuel Buena <sbuena@cs.stonybrook.edu>
2024-02-03 16:17:41 -0500

    edit server.js

--- src/server.js
+++ src/server.js
@@ -1,55 +1,52 @@
 const { MongoClient } = require("mongodb");
 const uri = 'mongodb+srv://hopper:hopperhellyeah@cluster0.jkkkjsb.mongodb.net/?retryWrites=true&w=majority';
 const client = new MongoClient(uri);
+const db = client.db('hopper');
 
 async function initDatabase() {
-    try {
-        const db = client.db('hopper');
-        const locationCollection = db.collection('locations');
-        const profileCollection = db.collection('profiles');
-        await profileCollection.createIndex( { username: 1 }, { unique: true } );
-        await locationCollection.createIndex( { locid: 1 }, { unique: true } );
-        await addLocation('wagner', 'Wagner Hall', 'Roosevelt');
-        await addLocation('greeley', 'Greeley Hall', 'Roosevelt');
-        await addLocation('keller', 'Keller Hall', 'Roosevelt');
-        await addLocation('yang', 'Yang Hall', 'Nobel');
-    } finally {
-        await client.close();
-    }
+    const locationCollection = db.collection('locations');
+    const profileCollection = db.collection('profiles');
+    await profileCollection.createIndex( { username: 1 }, { unique: true } );
+    await locationCollection.createIndex( { locid: 1 }, { unique: true } );
+    await addLocation('wagner', 'Wagner Hall', 'Roosevelt');
+    await addLocation('greeley', 'Greeley Hall', 'Roosevelt');
+    await addLocation('keller', 'Keller Hall', 'Roosevelt');
+    await addLocation('yang', 'Yang Hall', 'Nobel');
 }
 
 async function addLocation(locid, name, community) {
-    try {
-        const db = client.db('hopper');
-        const locationCollection = db.collection('locations');
-        await locationCollection.insertOne({
-            locid: locid,
-            name: name,
-            community: community,
-        });
-    } finally {
-        await client.close();
-    }
+    const locationCollection = db.collection('locations');
+    await locationCollection.replaceOne({
+        locid: locid,
+        name: name,
+        community: community,
+    });
 }
 
-async function addProfile(username, password, gender, major, desc) {
-  try {
-    const db = client.db('hopper');
+async function addProfile(username, password, gender, preferredGender, major, desc, locid, pictures) {
     const profileCollection = db.collection('profiles');
-    await profileCollection.insertOne({
+    await profileCollection.replaceOne({
         username: username,
         password: password,
         gender: gender,
+        preferredGender: preferredGender,
         major: major,
-        desc: desc
+        desc: desc,
+        locid: locid,
+        pictures: pictures
     });
-  } finally {
-    await client.close();
-  }
+}
+
+async function getProfiles() {
+    const profileCollection = db.collection('profiles');
+    let profiles = await profileCollection.findOne();
+    console.log(profiles);
 }
 
 async function run() {
+    await addProfile('hopper', 'hopper123', 'female', 'male', 'CSE', 'A fun bun!', 'wagner', ['hi.png']);
     await initDatabase();
-    await addProfile('hopper', 'hopper123', 'female', 'CSE', 'A fun bun!');
+    await getProfiles();
+    await client.close();
 }
 run().catch(console.dir);
