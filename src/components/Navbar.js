64d8f72514c5657bae552b640d2448b57dce82ec
eduardoloz <eddieslozano@gmail.com>
2024-02-03 13:15:10 -0500

    Maybe it renders finally please i beg please o god

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import '../css/navbar.css'
+
+const Navbar = () => {
+    return(
+    <div class="navbar">
+        <a href="#home">Home</a>
+        <a href="#about">About</a>
+        <a href="#contact">Contact</a>
+        <a href="#login" class="right">About Us</a>
+    </div>
+    );
+}
+
+export default Navbar;
