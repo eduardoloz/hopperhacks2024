ce34e3c32747f658cc8b09be42aa469b10b84fe5
eduardoloz <eddieslozano@gmail.com>
2024-02-03 16:44:16 -0500

    why is the thing broken

--- src/App.js
+++ src/App.js
@@ -2,18 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom"
+// import {
+//   createBrowserRouter,
+//   RouterProvider,
+// } from "react-router-dom"
 
 function App() {
-  const router = createBrowserRouter([
-    {
-      path: "/home",
-      element: <Home />,
-    },//loads <Home> when you visit home
-  ])
+  // const router = createBrowserRouter([
+  //   {
+  //     path: "/home",
+  //     element: <Home />,
+  //   },//loads <Home> when you visit home
+  // ])
 
   return (
     <div>
