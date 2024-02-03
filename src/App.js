edc3e64dfb9d547c132db8111c4f4d22f7a9c673
eduardoloz <eddieslozano@gmail.com>
2024-02-03 15:39:57 -0500

    lee's files

--- src/App.js
+++ src/App.js
@@ -2,8 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom"
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/home",
+      element: <Home />,
+    },//loads <Home> when you visit home
+  ])
+
   return (
     <div>
       <Navbar />
