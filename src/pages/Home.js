7e506024472825c866237e7aa1d9f54cf60ac861
LeeEisenberg <leeeisenberg05@gmail.com>
2024-02-03 18:16:54 -0500

    more button stuff

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,8 +6,10 @@ const Home = () => {
             <div className="page">
                 <div className="home">
                     <h1>❤ ❤ ❤ ~ Hopper Dreams ~ ❤ ❤ ❤</h1>
-                    <a href="https://www.example.com" role="button" class="button">Button Link 1</a>
-                    <a href="https://www.example.com" role="button" class="button">Button Link 2</a>
+                    <div className="buttons">
+                        <a href="https://www.example.com" role="button" class="button">Button Link 1</a>
+                        <a href="https://www.example.com" role="button" class="button">Button Link 2</a>
+                    </div>
                 </div>
             </div>
         </div>
