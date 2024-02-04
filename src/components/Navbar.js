419bfed7fdc799719f1216336ddb14c138b3495b
Jerry <103540592+NotJerwee@users.noreply.github.com>
2024-02-04 00:08:56 -0500

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,6 +6,7 @@ const Navbar = () => {
         <a href="/">Home</a>
         <a href="#about">About</a>
         <a href="/date">Date!</a>
+        <a href="#profile">Profile</a>
         <a href="#login" class="right">About Us</a>
     </div>
     );
