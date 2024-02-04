877a98188c9c99d011fca9b54231ff2d67c83fa7
eduardoloz <eddieslozano@gmail.com>
2024-02-04 02:01:30 -0500

    Fricked up the settings. Will fix soon hopefully.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from "./pages/Home";
 import DatingGrounds from "./pages/DatingGrounds";
 import Register from "./pages/register";
 import Login from "./pages/login";
+import Settings from "./pages/Settings";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
           <Route path="/date" element={<DatingGrounds />} />
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/settings" element={<Settings />} />
         </Routes>
       </Router>
     </div>
