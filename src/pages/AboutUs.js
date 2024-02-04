83c8e845a413e9d5b13180cbb0b0ee7942766206
Jerry Lin <jerrylin2488@gmail.com>
2024-02-04 03:09:49 -0500

    Update about us page

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -6,15 +6,18 @@ const AboutUs = () => {
             <div className="aboutus-text">
             <p>
                 Introducing "Hopper Dreams," the quintessential dating oasis crafted for the intellectually curious and romantically inclined computer science virtuosos at Stony Brook University. Conceived amidst the creative fervor of the Women in Computer Science Hackathon, Hopper Dreams is not merely a platform for finding love; it's a revolutionary crusade where the rhythmic beats of tech-savvy hearts merge with the logical elegance of programming.
-
+            </p>
+            <p>
                 Hopper Dreams transcends the traditional paradigms of matchmaking. In this digital utopia, your fervor for coding and innovation isn't just recognized—it's the main attraction. Picture a realm where cozy nights are spent unraveling the mysteries of algorithms together, and playful banter is about the latest in quantum computing.
-
+            </p>
+            <p>
                 Crafted by the visionary women in tech, our sophisticated matching algorithm ensures a journey that is not only smooth and secure but also deeply respectful and inclusive of every individual's story. Hopper Dreams is about creating meaningful connections that are rooted in the shared zeal for technology and the desire to make a difference in the digital world.
-
+            </p>
                 But Hopper Dreams is more than just about finding someone who gets your tech jokes. It's about connecting on a deeper level, where your coding practices, preferred technologies, and problem-solving approaches resonate with someone who understands. Our platform is designed to bring together minds that are not just compatible but also complementary, offering a foundation for relationships that go beyond the superficial.
-
+            <p>
                 And for those seeking a bit of adventure, Hopper Dreams offers themed coding challenges, collaborative hackathon-style dates, and engaging virtual meetups, redefining the essence of romantic pursuit. Whether you're a seasoned developer, an AI enthusiast, or a budding tech entrepreneur, Hopper Dreams welcomes you with open arms.
-
+            </p>
+            <p>
                 So, if the mainstream dating scene feels disconnected from your true self, if you yearn for a partner who not only understands but shares your passion for tech, Hopper Dreams is where your heart belongs. Embark on this exciting voyage with us, where each shared line of code could lead you to the partner who complements your tech-savvy soul. Dive into Hopper Dreams, where the quest for love and the pursuit of technological innovation converge, creating a tapestry of connections that are as profound as they are passionate.
             </p>
             </div>
