f3f2e70ff860e0b198397cba41482ff91587fa1c
Samuel Buena <sbuena@cs.stonybrook.edu>
2024-02-03 22:37:15 -0500

    fix bindings

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import { useNavigate } from "react-router-dom";
 import "../css/login.css";
-import { Link } from "react-router-dom";
 
 function Login() {
   return (
--- src/pages/register.js
+++ src/pages/register.js
@@ -5,31 +5,16 @@ import { useNavigate } from "react-router-dom";
 import hopperImage from "../assets/hopper.png"; 
 
 function Register() {
-  const [user, setUsers] = useState([])
-  const [email, setEmail] = useState('')
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const navigate = useNavigate();
 
-  useEffect(() => {
-    fetchUsers();
-  }, [])
-
-  const fetchUsers = () => {
-    axios.get("http://localhost:3001/register")
-    .then((res) => {
-      console.log(res.data)
-    })
-  }
-
   const handleRegister = (event) => {
     event.preventDefault();
-    axios.post('http://localhost:3001/register', { email, username, password}).then(() => {
+    axios.post('http://localhost:3001/register', { username, password }).then(() => {
       alert('Registration Sucesssful')
-      setEmail('')
       setUsername('')
       setPassword('')
-      fetchUsers()
       navigate('/login')
     }) .catch((error) => {
       console.log('Unable to register user')
@@ -39,14 +24,14 @@ function Register() {
   return (
     <div className="register-container">
       <div className="register-form">
-        <form action="/register" method="post" className="form">
+        <form onSubmit={handleRegister} className="form">
           <img class="register-img" src={hopperImage} alt="hopper" /> 
           <p className="register-title">Hopper Dreams Application</p>
           <div className="username-input">
-            <input type="text" placeholder="Enter Username" />
+            <input type="text" placeholder="Enter Username" value={username} onChange={(e) => setUsername(e.target.value)} />
           </div>
           <div className="password-input">
-            <input type="password" placeholder="Enter Password" />
+            <input type="password" placeholder="Enter Password" value={password} onChange={(e) => setPassword(e.target.value)} />
           </div>
           <button className="register-btn">❤❤❤ ~ Register ~ ❤❤❤</button>
           <p class="login">Already a hopper dreamer?&nbsp;<a href="/login">Login now!</a></p>
