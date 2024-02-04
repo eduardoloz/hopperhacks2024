b62df340f1f1f34d01faefb7708200fc2fc58310
Jerry Lin <jerrylin2488@gmail.com>
2024-02-04 03:21:46 -0500

    Update navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,9 +4,8 @@ const Navbar = () => {
     return(
     <div class="navbar">
         <a href="/">Home</a>
-        <a href="/">About</a>
+        <a href="/About-us">About us</a>
         <a href="/date">Date!</a>
-        <a href="/About-us" class="right">About Us</a>
         <a href="/settings" class="right">My Account</a> 
     </div>
     );
