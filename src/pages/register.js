917acffd8a6972e87383fbbdb472887e5d9e173c
Jerry <103540592+NotJerwee@users.noreply.github.com>
2024-02-03 19:22:53 -0500

    Update register.js

--- src/pages/register.js
+++ src/pages/register.js
@@ -1,22 +1,24 @@
 import React from "react";
 import "../css/register.css";
 import { Link } from "react-router-dom";
+import hopperImage from "../assets/hopper.png"; 
 
 function Register() {
   return (
-    <div class="container-register">
-    <div class="register-form">
-      <form action="/register" method="post" class="form">
-        <p class="register-title">Hopper Dreams Application</p>
-        <div class="username-input">
-          <input type="text" placeholder="Enter Username" />
-        </div>
-        <div class="password-input">
-          <input type="password" placeholder="Enter Password" />
-        </div>
-        <button class="register-btn">❤❤❤ ~ Register ~ ❤❤❤</button>
-      </form>
-    </div>
+    <div className="container">
+      <div className="register-form">
+        <form action="/register" method="post" className="form">
+          <img class="register-img" src={hopperImage} alt="hopper" /> 
+          <p className="register-title">Hopper Dreams Application</p>
+          <div className="username-input">
+            <input type="text" placeholder="Enter Username" />
+          </div>
+          <div className="password-input">
+            <input type="password" placeholder="Enter Password" />
+          </div>
+          <button className="register-btn">❤❤❤ ~ Register ~ ❤❤❤</button>
+        </form>
+      </div>
     </div>
   );
 }
